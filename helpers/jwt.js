ba9b425fd074f1f1cce8e8bbd04b5d02560c332a
maxhamz <maxwell.hamzah@gmail.com>
2020-04-13 23:33:49 +0700

    added some parallel code only to be unleased during testinf

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,10 +1,20 @@
 const jwt = require('jsonwebtoken')
 
 function createToken(payload) {
+
+    //FOR TEST
+    // return jwt.sign(payload, "rahasiadong", { expiresIn: '1h' })
+
+    //FOR DEVELOPMENT
     return jwt.sign(payload, process.env.SECRET, { expiresIn: '1h' })
 }
 
 function checkToken(payload) {
+
+    //FOR TEST
+    // return jwt.verify(payload, "rahasiadong")
+
+    //FOR DEVELOPMENT
     return jwt.verify(payload, process.env.SECRET)
 }
 
