b881069847dc621c108c4ff8d180b4ca3e5cfdb3
maxhamz <maxwell.hamzah@gmail.com>
2020-04-17 22:54:46 +0700

    used imgur in image create & update, First time using async await

--- controllers/product.js
+++ controllers/product.js
@@ -1,6 +1,7 @@
 const {Product} = require('../models')
 const {customError} = require('../helpers/customError')
 const {defaultPic} = require('../helpers/defaultPic')
+const {post2Imgur} = require('../helpers/tesimgur')
 let pic
 class ProductController {
 
@@ -9,6 +10,9 @@ class ProductController {
         // console.log(">>> CONTROLLERS: CREATE PRODUCT");
         const {name, description, category, image_url, price, stock} = req.body
         // console.log(req.body);
+        // console.log(req.body);
+        // console.log(req.headers);
+        // console.log(req.body);
 
         if(!image_url) {
             pic = defaultPic(category)
@@ -16,22 +20,34 @@ class ProductController {
             pic = image_url
         }
 
-        return Product.create({
-            name: name,
-            description: description,
-            category: category,
-            image_url: pic,
-            price: price,
-            stock: stock
-        })
-        .then(response => {
-            // console.log("PRODUCT ADDED TO INVENTORY");
-            // console.log(response);
-            return res.status(201).json(response)
-        })
-        .catch(err => {
-            return next(err)
-        })
+        return post2Imgur(pic)
+            .then(response => {
+                // console.log("WHASSAP BLIMPO?");
+                // console.log(response);
+
+                if(!(response instanceof Error)) {
+                    return Product.create({
+                        name: name,
+                        description: description,
+                        category: category,
+                        image_url: response,
+                        price: price,
+                        stock: stock
+                    })
+                } else {
+                    throw new customError(400, 'IMAGE UPLOAD FAILED')
+                }
+
+                
+            })
+            .then(response => {
+                // console.log("PRODUCT ADDED TO INVENTORY");
+                // console.log(response);
+                return res.status(201).json(response)
+            })
+            .catch(err => {
+                return next(err)
+            })
 
     }
 
@@ -94,23 +110,45 @@ class ProductController {
                 } else {
                     pic = image_url
                 }
-        
-                return Product.update({
-                    name: name,
-                    description: description,
-                    category: category,
-                    image_url: pic,
-                    price: price,
-                    stock: stock
-                }, {
-                    where: {
-                        id: req.params.id
-                    },
-                    returning: true
-                })
+
+                let imguregex = /imgur/gi
+                let imgurd = pic.match(imguregex)
+
+                console.log(imgurd);
+
+                if(!imgurd) {
+                    return post2Imgur(pic)
+                } else {
+                    return pic
+                }
                 
+    
             }
         })
+        .then(response => {
+                // console.log("WHASSAP BLIMPO?");
+                // console.log(response);
+
+                if(!(response instanceof Error)) {
+                    return Product.update({
+                        name: name,
+                        description: description,
+                        category: category,
+                        image_url: response,
+                        price: price,
+                        stock: stock
+                    }, {
+                        where: {
+                            id: req.params.id
+                        },
+                        returning: true
+                    })
+                } else {
+                    throw new customError(400, 'IMAGE UPLOAD FAILED')
+                }
+
+                
+            })
         .then(response => {
             // console.log("PRODUCT UPDATED");
             return res.status(200).json(response[1][0])
---
+++ helpers/tesimgur.js
@@ -0,0 +1,63 @@
+const fetch = require('node-fetch')
+global.Headers = fetch.Headers;
+const FormData = require('form-data')
+// let sampleURL = "https://pbs.twimg.com/profile_images/679852624754028544/Inq6KagO_reasonably_small.jpg"
+// const {customError} = require('./customError')
+
+async function post2Imgur(baseURL) {
+
+    let link
+
+    var myHeaders = new Headers();
+
+    // let basecid = "Client-ID "
+    // let mycid = process.env.IMGUR_CLIENT_ID
+
+    // let cid = basecid+mycid
+
+    myHeaders.append("Authorization", "Client-ID 390277fdd42f174");
+    // myHeaders.append("Authorization", cid);
+
+    var formdata = new FormData();
+    formdata.append("image", baseURL);
+
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: formdata,
+        redirect: 'follow'
+    };
+
+
+    link = await fetch("https://api.imgur.com/3/image", requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            console.log("OK, THIS IS IMGUR'S URL");
+            // console.log(result)
+            let d = JSON.parse(result)
+            console.log(d.data.link);
+            return d.data.link
+        })
+        .catch(error => {
+            // console.log('error', error)
+            console.log("ERROR POSTING 2 IMGUR");
+            console.error(error)
+            // next(error)
+        });
+
+    return link
+}
+
+
+// console.log("HELLO");
+// let s = post2Imgur(sampleURL)
+// console.log(s);
+
+// s
+// .then(result => {
+//     console.log("GERONIMO!");
+//     console.log(result);
+// })
+
+module.exports = {post2Imgur}
