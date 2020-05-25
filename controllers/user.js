a7ff3bf68e670e00630c89eba887a1a3b3c758a3
maxhamz <maxwell.hamzah@gmail.com>
2020-05-25 22:21:00 +0700

    successfully create googleLogin

--- controllers/user.js
+++ controllers/user.js
@@ -2,15 +2,22 @@ const {User} = require('../models')
 const {customError} = require('../helpers/customError')
 const {checkPassword} = require('../helpers/bcrypt')
 const {createToken} = require('../helpers/jwt')
+const {
+    OAuth2Client
+} = require('google-auth-library')
+
+const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENTID);
 let payload
 let token
+let emailAddress
+let defRole = 'user'
 
 class UserController {
 
     static register(req, res, next) {
         // console.log(">>> CONTROLLERS - USER - REGISTER");
         // console.log(req.body);
-        let defRole
+        defRole
         const {email, password, role} = req.body
         if(!role) {
             defRole = 'user'
@@ -74,14 +81,87 @@ class UserController {
     }
 
     static fetchAll(req, res, next) {
-        console.log("FETCHING ALL");
+        // console.log("FETCHING ALL");
         User.findAll()
             .then(response => {
-                console.log("ALL USERS FETCHED");
+                // console.log("ALL USERS FETCHED");
                 res.status(200).json({data: response})
             })
             .catch(err => {
                 next(err)
+            })
+    }
+
+
+    static googleLogin(req, res, next) {
+        // console.log("GOOGLE LOGIN @ CONTROLLERS");
+        // console.log(req.headers)
+
+        token = req.headers.access_token
+
+
+
+        return googleClient.verifyIdToken({
+                idToken: token,
+                audience: process.env.GOOGLE_CLIENTID
+            })
+            .then(ticket => {
+                // console.log("OK WHAT'S TICKET'S PAYLOAD?");
+                // console.log(ticket);
+
+                payload = ticket.getPayload();
+                // console.log(payload);
+
+                // // let userId = payload['sub']
+                emailAddress = payload['email']
+
+                // console.log("THIS IS USER GOOGLE EMAIL WE'RE GONNA FIND?");
+                // console.log(emailAddress);
+
+                return User.findOne({
+                    where: {
+                        email: emailAddress
+                    }
+                })
+
+
+            })
+            .then(response => {
+                // console.log("WHAT'S VERDICT FOR FINDING USER?");
+                // console.log(response);
+                if (response) {
+                    // console.log("THIS IS RETURNING USER");
+                    return response
+                } else {
+                    // console.log("THIS IS NEW USER");
+                    // console.log("THIS IS PROCESS ENV");
+                    // console.log(process.env);
+                    defRole = 'user'
+                    return User.create({
+                        email: emailAddress,
+                        password: process.env.SECRET,
+                        role: defRole
+                    })
+                }
+            })
+            .then(response => {
+
+                payload = {
+                    id: response.id,
+                    email: response.email,
+                    role: response.role
+                }
+
+                token = createToken(payload)
+
+                return res.status(200).json({
+                    access_token: token,
+                    id: response.id,
+                    email: response.email
+                })
+            })
+            .catch(err => {
+                return next(err)
             })
     }
 
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,8 @@ const UserController = require('../controllers/user')
 
 userRoutes.post('/register', UserController.register)
 userRoutes.post('/login', UserController.login)
+userRoutes.post('/googleLogin', UserController.googleLogin)
 userRoutes.get('/', UserController.fetchAll)
+
 
 module.exports = userRoutes
